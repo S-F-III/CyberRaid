3e022073fb430d8de94573a14551e6defe3134a9
Steve Fleming <mediocreacorn@gmail.com>
2024-10-27 08:16:05 -0500

    Update InternetCafeEvent.java

--- src/application/objects/InternetCafeEvent.java
+++ src/application/objects/InternetCafeEvent.java
@@ -3,11 +3,11 @@ public class InternetCafeEvent implements Events {
 
 	@Override
 	public void startEvent() {
-	System.out.println("Unable to think of a place to go you wander for a while before stumbling into an internet cafe.\n\"Time to figure out who those creeps were\"\n...\n...\n...\nMethodically combing through some new history and trying to identify the men who jumped you leads to a better understanding of Digital Forensics!!!\nSomething those creeps said jumped out at you,\n\"We have been watching you...\"\nThat could only mean one thing...those men must be part of an Advanced Persistent Threat!\nThe computers around you immediately shut off and are replaced by an unfamiliar OS...\n
+	Drawable.typeText("Unable to think of a place to go you wander for a while before stumbling into an internet cafe.\n\"Time to figure out who those creeps were\"\n...\n...\n...\nMethodically combing through some new history and trying to identify the men who jumped you leads to a better understanding of Digital Forensics!!!\nSomething those creeps said jumped out at you,\n\"We have been watching you...\"\nThat could only mean one thing...those men must be part of an Advanced Persistent Threat!\nThe computers around you immediately shut off and are replaced by an unfamiliar OS...\n
 	}
 
 	public void endEvent() {
-	System.out.println("\"These fights are getting tougher, I had better finish this quickly\"\nNow that the BotNet has been dealt with you can continue your research.");
+	Drawable.typeText("\"These fights are getting tougher, I had better finish this quickly\"\nNow that the BotNet has been dealt with you can continue your research.");
 	}
 
 
