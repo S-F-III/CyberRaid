4c1f9a0ad1dd8c1a650c5da334061abcccd2ce55
Steve Fleming <mediocreacorn@gmail.com>
2024-10-27 08:19:35 -0500

    Update scriptKiddieEvent.java

--- src/application/objects/scriptKiddieEvent.java
+++ src/application/objects/scriptKiddieEvent.java
@@ -4,12 +4,12 @@ public class ScriptKiddieFight implements Events{
 
 	@Override
 	public void eventStart() {
-	System.out.println("You have returned to the past! It seems they haven't thought of everything. \n...\n...\n\"This cant be the same place I just visited\" You turn around and see a disheveled man with a wicked grin.") 
+	Drawable.typeText("You have returned to the past! It seems they haven't thought of everything. \n...\n...\n\"This cant be the same place I just visited\" You turn around and see a disheveled man with a wicked grin.") 
 	}
 
 	@Override
 	public void eventEnd() {
-	System.out.println("\"It's 1985, how are there script kiddies?!\" Rummaging through some of the data you pulled from him you gain a better understanding of Threat Intelligence!!! and you find references to a Data Center and Small Business that could be of interest.") 
+	Drawable.typeText("\"It's 1985, how are there script kiddies?!\" Rummaging through some of the data you pulled from him you gain a better understanding of Threat Intelligence!!! and you find references to a Data Center and Small Business that could be of interest.") 
 	}
 
 }
