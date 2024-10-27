4a76d3065f10f14afc05c3a4d6347246a832db58
zacharysmartt <54597383+zacharysmartt@users.noreply.github.com>
2024-10-27 09:46:30 -0500

    Update SmallBusinessEvent.java

--- src/application/objects/SmallBusinessEvent.java
+++ src/application/objects/SmallBusinessEvent.java
@@ -1,16 +1,13 @@
 package application.objects;
+
 public class SmallBusinessEvent implements Events {
 
-	@Override
-	public void startEvent() {
+	public static void startEvent() {
 	Drawable.typeText("Checking the address you confirm its got to be the right place.\nThe sign reads \"ByteBack Technologies\"\nLooking around you shrug and head inside...\n...\n...\nYou eventually find a terminal that was left unlocked and begin to research why this location would have been in that script kiddie's files.\nDuring your reseach you gain a better understanding of Secure Coding Practices!!!\nA notification pings your terminal. You take some time to read the incoming message...");
 	}
 	
-	@Override
-	public void endEvent() {
+	public static void endEvent() {
 	Drawable.typeText("Rolling your eyes at the loss of time you close the terminal.\n\"Time to head out\"");
 	}
-
-
 
 }
