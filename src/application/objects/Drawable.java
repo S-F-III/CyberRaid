a5b0532a8d9a310ec5531b034305c8027296ab6c
Steve Fleming <mediocreacorn@gmail.com>
2024-10-27 09:09:59 -0500

    Update Drawable.java

--- src/application/objects/Drawable.java
+++ src/application/objects/Drawable.java
@@ -13,8 +13,23 @@ public class Drawable {
         }
         System.out.println();
     }
-
-  
-
-
+    public static void typeTextFaster(String text) {
+        for (char c : text.toCharArray()) {
+            System.out.print(c);
+            try {
+                Thread.sleep(10); // Delay in milliseconds (adjust as needed)
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt(); // Restore interrupted status
+                break;
+            }
+        }
+        System.out.println();
+    }
+  public static void gameOverText()
+  Drawable.typeTextFaster(" _____   ___  ___  ___ _____   _____  _   _ ___________ _ _ _ ");
+  Drawable.typeTextFaster("|  __ \\ / _ \\ |  \\/  ||  ___| |  _  || | | |  ___| ___ \\ | | |");
+  Drawable.typeTextFaster("| |  \\// /_\\ \\| .  . || |__   | | | || | | | |__ | |_/ / | | |");
+  Drawable.typeTextFaster("| | __ |  _  || |\\/| ||  __|  | | | || | | |  __||    /| | | |");
+  Drawable.typeTextFaster("| |_\\ \\| | | || |  | || |___  \\ \\_/ /\\ \\_/ / |___| |\\ \\|_|_|_|);
+  Drawable.typeTextFaster(" \\____/\\_| |_/\\_|  |_/\\____/   \\___/  \\___/\\____/\\_| \\_(_|_|_)");
 }
