39624832c233c2127a1a0e1ca9a8ee30fbc7753d
zacharysmartt <54597383+zacharysmartt@users.noreply.github.com>
2024-10-27 09:47:24 -0500

    Update and rename dataCenterEvent.java to DataCenterEvent.java

---
+++ src/application/objects/DataCenterEvent.java
@@ -0,0 +1,14 @@
+package application.objects;
+
+public class DataCenterEvent implements Events {
+	
+	public static void startEvent() {
+		Drawable.typeText("You make your way to the Data Center.\nWatching some of the emplyees you notice that some let people in without swiping their card.\n Taking a deep breath you start walking towards the center with a small group of people.\nSure enough they let you in without a word.\n...\n...\nAfter looking through some of the files you gain some knowledge towards Risk Assessment and Management!!!\n\"That should prove useful\"\nSuddenly you feel a tap on your shoulder. Spinning around, you see a woman removing an unconvincing disguise.\n\"Get out of my way, this information belongs on the net\""); 
+	}
+	
+	public static void endEvent() {
+	Drawable.typeText("An Alarm starts to screech overhead.\n\"Time to go!\" Making your way out of the building you manage to slip off before law enforcement could arrive.");
+		
+	}
+
+}
