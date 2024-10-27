61d8de0604afa95816411b445e13a938cf06e40c
Steve Fleming <mediocreacorn@gmail.com>
2024-10-27 08:17:47 -0500

    Update SmallBusinessEvent.java

--- src/application/objects/SmallBusinessEvent.java
+++ src/application/objects/SmallBusinessEvent.java
@@ -3,12 +3,12 @@ public class SmallBusinessEvent implements Events {
 
 	@Override
 	public void startEvent() {
-	System.out.println("Checking the address you confirm its got to be the right place.\nThe sign reads \"ByteBack Technologies\"\nLooking around you shrug and head inside...\n...\n...\nYou eventually find a terminal that was left unlocked and begin to research why this location would have been in that script kiddie's files.\nDuring your reseach you gain a better understanding of Secure Coding Practices!!!\nA notification pings your terminal. You take some time to read the incoming message...");
+	Drawable.typeText("Checking the address you confirm its got to be the right place.\nThe sign reads \"ByteBack Technologies\"\nLooking around you shrug and head inside...\n...\n...\nYou eventually find a terminal that was left unlocked and begin to research why this location would have been in that script kiddie's files.\nDuring your reseach you gain a better understanding of Secure Coding Practices!!!\nA notification pings your terminal. You take some time to read the incoming message...");
 	}
 	
 	@Override
 	public void endEvent() {
-	System.out.println("Rolling your eyes at the loss of time you close the terminal.\n\"Time to head out\"");
+	Drawable.typeText("Rolling your eyes at the loss of time you close the terminal.\n\"Time to head out\"");
 	}
 
 
