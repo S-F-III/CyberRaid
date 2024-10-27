5f8ce3f34b6fc4bbed640d5ff078d99634b60f45
zacharysmartt <54597383+zacharysmartt@users.noreply.github.com>
2024-10-27 09:44:37 -0500

    Update RipOffEvent.java

--- src/application/objects/RipOffEvent.java
+++ src/application/objects/RipOffEvent.java
@@ -1,9 +1,13 @@
 package application.objects;
+
 public class RipOffEvent implements Events {
-	public void startEvent() {
-	Drawable.typeText("Still shocked that you found someone with knowledge on your attackers you prepare yourself for an ambush.\nSure enough the very same script kiddie you bested appeared out of the shadows.\n\"Do you really think things are going to go differently this time?\"");
-       
-       public void endEvent() {
-  	Drawable.typeText("Trying to limp away you fall to your knees only to be kicked over by your foe.\n\"The malevolent expression on his face as he gloats over you is all you can focus on.\nRed and blue lights start to cast shadows on the wall behind him and the script kiddie's expression changes to fear.\nRunning off he leaves you alone in the alley to fend for yourself.\nSome good did come of this, even if it was painful...the script kiddie had to have gotten that code from THEM directly meaning you should be able to set up a buy...");
-	}	
+	
+	public static void startEvent() {
+		Drawable.typeText("Still shocked that you found someone with knowledge on your attackers you prepare yourself for an ambush.\nSure enough the very same script kiddie you bested appeared out of the shadows.\n\"Do you really think things are going to go differently this time?\"");
+	} 
+	
+	public static void endEvent() {
+    	Drawable.typeText("Trying to limp away you fall to your knees only to be kicked over by your foe.\n\"The malevolent expression on his face as he gloats over you is all you can focus on.\nRed and blue lights start to cast shadows on the wall behind him and the script kiddie's expression changes to fear.\nRunning off he leaves you alone in the alley to fend for yourself.\nSome good did come of this, even if it was painful...the script kiddie had to have gotten that code from THEM directly meaning you should be able to set up a buy...");
+	}
+
 }
